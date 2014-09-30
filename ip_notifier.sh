c7ec85dc997d9c3d623309acee8b0bef59e10043
Patrick Jennings <patrickaeb@gmail.com>
2014-09-29 20:33:15 -0400

    defining email address as a var. using subject of email.

--- ip_notifier.sh
+++ ip_notifier.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+EMAIL_ADDRESS='example@gmail.com'
 CURRENT_IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 FILE_NAME='/home/patrick/.wan_ip'
 
@@ -10,7 +11,8 @@ else
 fi
 
 if [ "$CURRENT_IP" != "$PREVIOUS_IP" ] ; then
-    echo "IP Address Change: $CURRENT_IP" | msmtp -a default example@gmail.com
+    echo -e "Subject: IP Address Change: $CURRENT_IP" |
+        msmtp -a default $EMAIL_ADDRESS
 fi
 
 echo "$CURRENT_IP" > "$FILE_NAME"
